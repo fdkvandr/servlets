378031f1ef2f1a1cd14caae310965f9c0370afb6
andrey.fedyakov <andrey.fedyakov@glowbyteconsulting.com>
2022-10-05 17:00:28 +0300

    v1.5.0 Getting body

--- src/com/http/servlet/FirstServlet.java
+++ src/com/http/servlet/FirstServlet.java
@@ -7,10 +7,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
+import java.util.stream.Stream;
 
 @WebServlet("/first") // Аннотация, которая описывает наш сервлет
 public class FirstServlet extends HttpServlet {
@@ -37,7 +39,10 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getParameterMap();
+        try (BufferedReader reader = req.getReader();
+             Stream<String> lines = reader.lines()) {
+            lines.forEach(System.out::println);
+        }
     }
 
     @Override
