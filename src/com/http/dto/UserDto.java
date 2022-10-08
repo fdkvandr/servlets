12c1d5d8c2fd878d3a8799fd0ea6f9454ba20ca3
andrey.fedyakov <andrey.fedyakov@glowbyteconsulting.com>
2022-10-08 16:48:39 +0300

    v1.11.0 Using attributes of session

---
+++ src/com/http/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.http.dto;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class UserDto {
+
+    Long id;
+    String mail;
+}
--- src/com/http/servlet/SessionServlet.java
+++ src/com/http/servlet/SessionServlet.java
@@ -1,5 +1,6 @@
 package com.http.servlet;
 
+import com.http.dto.UserDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,9 +13,18 @@ import java.io.IOException;
 @WebServlet("/sessions")
 public class SessionServlet extends HttpServlet {
 
+    private static final String USER = "user";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        System.out.println(session.isNew());
+        UserDto user = (UserDto) session.getAttribute(USER);
+        if (user == null) {
+            user = UserDto.builder()
+                    .id(25L)
+                    .mail("test@gmail.com")
+                    .build();
+            session.setAttribute(USER, user);
+        }
     }
 }
