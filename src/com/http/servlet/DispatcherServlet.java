fcce884c2be51235304ac751294044513bb6d25e
andrey.fedyakov <andrey.fedyakov@glowbyteconsulting.com>
2022-10-08 18:17:11 +0300

    v1.12.0 Using forward/include/redirect

---
+++ src/com/http/servlet/DispatcherServlet.java
@@ -0,0 +1,35 @@
+package com.http.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/dispatcher")
+public class DispatcherServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+//        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/flights");
+//        req.setAttribute("1", "fiestObject");
+//        requestDispatcher.forward(req, resp);
+
+//        getServletContext().getRequestDispatcher("/flights").forward(req, resp);
+
+//        req.getRequestDispatcher("/flights").include(req, resp);
+//
+//        resp.getWriter().write("dsf");
+
+        resp.sendRedirect("/flights");
+
+        System.out.println("sfsa");
+
+
+
+    }
+}
