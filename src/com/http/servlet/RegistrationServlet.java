9a59ae27614b1b4321296f926c1d8f8003560cb4
andrey.fedyakov <andrey.fedyakov@glowbyteconsulting.com>
2022-10-13 23:12:18 +0300

    v1.21.0 Servlet Filters

--- src/com/http/servlet/RegistrationServlet.java
+++ src/com/http/servlet/RegistrationServlet.java
@@ -12,11 +12,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.Part;
 
 import java.io.IOException;
 
-@WebServlet("/registration")
+@WebServlet(value = "/registration", name = "RegistrationServlet")
 @MultipartConfig(fileSizeThreshold = 1024 * 1024)
 public class RegistrationServlet extends HttpServlet {
 
